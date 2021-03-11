cac81f6e0edcf9ae618b49e4471f306dd7e08f01
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-11 13:15:29 -0300

    create configs utils

---
+++ utils/horizonUtils.js
@@ -0,0 +1,18 @@
+let config;
+try {
+    config = require("../config.json");
+} catch (error) {
+    config = null;
+}
+
+exports.messages = {
+    rules: config ? config.rules : process.env.RULES,
+    tasks: config ? config.tasks : process.env.TASKS,
+    games: {
+        valorant: config ? config.games.valorant : process.env.VALORANT,
+        leagueoflegends: config
+            ? config.games.leagueoflegends
+            : process.env.LEAGUEOFLEGENDS,
+        ragnarok: config ? config.games.ragnarok : process.env.RAGNAROK,
+    },
+};
